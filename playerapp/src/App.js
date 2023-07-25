8d7fc11b5b10786e03de5920a7b6a56c34ac7ea6
As Salih <thickino@gmail.com>
2023-07-25 17:05:56 +0000

    first commit

---
+++ playerapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Players from "./Components/Players";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Players/>
+      <h1>The Players</h1>
+    </div>
+  );
+}
+
+export default App;
